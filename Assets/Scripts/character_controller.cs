4d5d9a5276bcc045abfc78b9c9a9fe1150e86e05
Numb <ironguard.kraynovalexander@gmail.com>
2017-03-28 19:49:57 +0300

    F! Добавлено приближение к объекту следования камерой при превышении определенных углов F! Добавлен изинг вращения объекта к вращению камеры F! Добавлены комментарии к character_controller.cs и camera_controller.cs B! Исправлена ошибка, при которой персонаж двигался по диагонали быстрее чем по горизонтали или вертикали

--- Assets/Scripts/character_controller.cs
+++ Assets/Scripts/character_controller.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 public class character_controller : MonoBehaviour {
+    //character horizontal speed
     public float speed = 5.0f;
+    //vertical speed of jumps
     public float jumpspeed = 10.0f;
+    //vertical negative speed of gravity
     public float gravity = 20.0F;
+    //vector of character's movement
     private Vector3 move_direction = Vector3.zero;
-    
-
+    //easing of the rotation controlled by a camera
+    public float rotation_easing;
 
     void Start () {
        
@@ -16,23 +20,37 @@ public class character_controller : MonoBehaviour {
 	
 	
 	void Update () {
-       
+        
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
-        {
-            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        {   //getting input
+            float v_input = Input.GetAxis("Vertical");
+            float h_input = Input.GetAxis("Horizontal");
+            //fixing a notorious diagonal bug
+            if ((h_input*v_input)!=0)
+            {
+                h_input /= (float)Math.Sqrt(2);
+                v_input /= (float)Math.Sqrt(2);
+            }
+            move_direction = new Vector3(h_input, 0, v_input);
+
             move_direction = transform.TransformDirection(move_direction);
+            //multiplying by speed
             move_direction *= speed;
+            //adding vertical jump
             if (Input.GetButton("Jump"))
                 move_direction.y = jumpspeed;
 
         }
+        //adding gravity
         move_direction.y -= gravity * Time.deltaTime;
-        Quaternion rotation  = Camera.main.transform.rotation;
+        //rotation controlled by the camera
+        Quaternion rotation = Camera.main.transform.rotation;
         rotation.x = 0;
         rotation.z = 0;
-        transform.rotation = rotation;
+        //adding easing to rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotation_easing * Time.deltaTime);
         controller.Move(move_direction * Time.deltaTime);
-
+        
     }
 }
