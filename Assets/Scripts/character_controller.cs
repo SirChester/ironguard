96cb099feb293aef00c9cb80a1495a30d500b422
Numb <ironguard.kraynovalexander@gmail.com>
2017-03-29 13:40:19 +0300

    !F Добавлен дампинг движения

--- Assets/Scripts/character_controller.cs
+++ Assets/Scripts/character_controller.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 public class character_controller : MonoBehaviour {
-    //character horizontal speed
-    public float speed = 5.0f;
+    //character "stat" speed
+    public float max_speed;
+    //current character horizontal and vertical  speed
+    private float v_speed;
+    private float h_speed;
     //vertical speed of jumps
     public float jumpspeed = 10.0f;
     //vertical negative speed of gravity
@@ -13,30 +16,71 @@ public class character_controller : MonoBehaviour {
     private Vector3 move_direction = Vector3.zero;
     //easing of the rotation controlled by a camera
     public float rotation_easing;
+    //temp var for vertical and horizontal damp (in terms of oXZ)
+    private float damp_velocity_v = 0.0f;
+    private float damp_velocity_h = 0.0f;
+
+
+    //temp var for tracking the last input from player
+    private float last_v_input = 0;
+    private float last_h_input = 0;
+    //time in seconds for damping
+    public float damp_time;
+
+    private float v_input;
+    private float h_input;
 
     void Start () {
-       
+        
     }
 	
 	
 	void Update () {
-        
+    
+
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
-        {   //getting input
-            float v_input = Input.GetAxis("Vertical");
-            float h_input = Input.GetAxis("Horizontal");
-            //fixing a notorious diagonal bug
-            if ((h_input*v_input)!=0)
-            {
-                h_input /= (float)Math.Sqrt(2);
-                v_input /= (float)Math.Sqrt(2);
-            }
-            move_direction = new Vector3(h_input, 0, v_input);
+        {
 
-            move_direction = transform.TransformDirection(move_direction);
+            v_input = 0;
+            h_input = 0;
+            //getting input
+            if ((Mathf.Abs(Input.GetAxis("Vertical")) == 1))
+            {
+               v_input = Input.GetAxis("Vertical");
+               //damping vertical speed
+               v_speed = Mathf.SmoothDamp(v_speed, max_speed, ref damp_velocity_v, damp_time);
+                //checking for a sharp turn
+                if (last_v_input == (v_input * (-1)))
+                    v_speed = 0;
+                last_v_input = v_input;
+            }else
+            {   // damping to zero when no input
+                v_speed = Mathf.SmoothDamp(v_speed, 0, ref damp_velocity_v, damp_time);
+            }
+            //same idea
+            if ((Mathf.Abs(Input.GetAxis("Horizontal")) == 1))
+            {
+                h_input = Input.GetAxis("Horizontal");
+                h_speed = Mathf.SmoothDamp(h_speed, max_speed, ref damp_velocity_h, damp_time);
+                if (last_h_input == (h_input * (-1)))
+                    h_speed = 0;
+                last_h_input = h_input;
+            }
+            else
+            {
+                h_speed = Mathf.SmoothDamp(h_speed, 0, ref damp_velocity_h, damp_time);
+            }
+           
+            // implementing movement
+            move_direction = new Vector3(last_h_input*h_speed, 0, last_v_input*v_speed);
+            //fixing a notorious diagonal bug
+            if ((h_input * v_input) != 0)
+            { move_direction /= Mathf.Sqrt(2); }
+
+                move_direction = transform.TransformDirection(move_direction);
             //multiplying by speed
-            move_direction *= speed;
+            
             //adding vertical jump
             if (Input.GetButton("Jump"))
                 move_direction.y = jumpspeed;
